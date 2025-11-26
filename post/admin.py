cbbc9dfeacffe7e7043ca6b3deba5abef2df3cab
SParb <ryuchigo@gmail.com>
2025-11-26 11:45:32 +0000

    add more admin functionality on userposts using summernote

--- post/admin.py
+++ post/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 from .models import UserPost, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+@admin.register(UserPost)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'author', 'created_on')
+    search_fields = ['title','content']
+    list_filter = ('created_on',)
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
 
 # Register your models here.
-admin.site.register(UserPost)
+
 admin.site.register(Comment)
